5141b63011c6a4a3295e043c88aa1745fc4928fb
BahodirDev <mydatabases714@gmail.com>
2023-03-08 18:18:45 +0500

    users page

--- src/Pages/users/User.jsx
+++ src/Pages/users/User.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import icon from "../../utils/icons/users.png";
+import "./users.css";
 
 function User() {
-    return (
-        <h3>Users</h3>
-    )
+  const data = [
+    {
+      user_id: 1,
+      user_firstname: "bir",
+      user_lastname: "bir",
+      user_img: icon,
+      user_email: "test@test.com",
+      user_createdat: "2023 - 03 - 04",
+      role_id: { role_name: "Dasturchi" },
+    },
+
+    {
+      user_id: 1,
+      user_firstname: "bir",
+      user_lastname: "bir",
+      user_img: icon,
+      user_email: "test@test.com",
+      user_createdat: "2023 - 03 - 04",
+      role_id: { role_name: "Dasturchi" },
+    },
+    // {
+    //   user_id: 2,
+    //   user_firstname: "bir",
+    //   user_lastname: "bir",
+    //   user_img: icon,
+    //   user_email: "test@test.com",
+    //   user_createdat: "2023 - 03 - 04",
+    //   role_id: { role_name: "Dasturchi" },
+    // },
+    {
+      user_id: 3,
+      user_firstname: "bir",
+      user_lastname: "bir",
+      user_img: icon,
+      user_email: "test@test.com",
+      user_createdat: "2023 - 03 - 04",
+      role_id: { role_name: "Dasturchi" },
+    },
+  ];
+  return (
+    <table class="user_ul">
+      <thead className="first_child ">
+        <tr>
+          <th>Hodim</th>
+          <th>Email</th>
+          <th>Qabul qilingan sana</th>
+          <th>Kasb</th>
+          <th>Tahrirlash</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((person) => {
+          return (
+            <tr className="user_list">
+              <td className="item_show">
+                <img src={icon} alt="avatar" />
+              </td>
+              <td className="item_show">{person.user_email}</td>
+              <td className="item_show">{person.user_createdat.toString()}</td>
+              <td className="item_show">{person.role_id.role_name}</td>
+              <td className="item_show">
+                <button className="user_list_green">Tahrirlash</button>
+                <button className="user_list_red">O'chirish</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+
+      <tr className="user_list_extra">
+            <td className="item_show"></td>
+          </tr>
+    </table>
+  );
 }
 
-export default User
+export default User;
--- src/components/menu/Leftbar.jsx
+++ src/components/menu/Leftbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import fourSquare from '../../utils/icons/four_square.png'
+import phone from '../../utils/icons/phone.png'
+import users from '../../utils/icons/users_white.png'
 
 function Leftbar() {
-  return <div className="left_bar">Left bar</div>;
+
+  let btsMass =[
+    {id:1,icon:fourSquare,title:"Asosiy menu",link:""},
+    {id:2,icon:phone,title:"Filiallar",link:""},
+    {id:3,icon:users,title:"Xodimlar",link:""},
+  ]
+
+  return (
+    <div className="left_bar">
+      <div className="logo">
+        <p id="logo">LOGO</p>
+      </div>
+      <div className="btns">
+        {
+          btsMass?.map((s)=>{
+            
+            return(
+              <NavLink className="btn_navigate btn " key={s.id} >
+                <img src={s.icon} alt="icons" className="icons_img mx-2" />
+                <span>{s.title}</span>
+              </NavLink>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
 }
 
 export default Leftbar;
